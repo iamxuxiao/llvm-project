93665d311079ce8e6f3ff3c0117e6be619843dd5
xiaoxu <xiaox@xmotors.ai>
2022-09-25 17:36:21 -0700

    if(lhs ==nullptr) matching nullptr and lhs

---
+++ bazel_tidy_example/raw_tree.cpp
@@ -0,0 +1,27 @@
+#include "helper.hpp"
+#include <iostream>
+
+void foo(int b){
+ int c;
+ int a = 10;
+ void* d;
+ if (a == 1){
+    c = a;
+ }
+ if (a == 2){
+    c = a;
+  }
+ if (a == 2){
+    c = a;
+  }
+
+  if (d == nullptr){
+    c = a;
+  }
+
+}
+
+int main(int argc, char** argv) {
+  helper();
+  return 0;
+}
