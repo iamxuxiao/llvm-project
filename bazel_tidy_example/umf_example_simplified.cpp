cbee5c3744ec199fb2dff976506250f30e79024d
xiaoxu <xiaox@xmotors.ai>
2022-10-22 15:58:20 -0700

    add optional check

--- bazel_tidy_example/umf_example_simplified.cpp
+++ bazel_tidy_example/umf_example_simplified.cpp
@@ -1,3 +1,5 @@
+#include <optional>
+
 struct Lane{};
 
 Lane* GetMapLaneById(const int Id){
@@ -8,19 +10,41 @@ Lane* GetMapLaneById(const int Id){
   }
 }
 
+std::optional<Lane> GetMapLaneByIdImproved(const int Id){
+  if(Id<0){
+    return std::nullopt;
+  }else{        
+    return std::optional<Lane>();
+  }
+}
+
+
 void MapFusionWork(){
 
-  const auto lane_ptr_maybe = GetMapLaneById(10);
-  if (lane_ptr_maybe){
+  const auto lane_ptr = GetMapLaneById(10);
+  if (lane_ptr){ // warn here 
     // nullcheck , maybe early exit 
   }
 
   
-  const auto another_lane_ptr_maybe = GetMapLaneById(-10);
-  if (another_lane_ptr_maybe == nullptr){
+  const auto another_lane_ptr = GetMapLaneById(-10);
+  if (another_lane_ptr == nullptr){ // no warn here
     // nullcheck , maybe early exit 
   }
 
+
+  const auto lane_ptr_maybe = GetMapLaneByIdImproved(10);
+  if (lane_ptr_maybe){  // warn here 
+    // nullcheck , maybe early exit 
+  }
+//
+//  
+  const auto another_lane_ptr_maybe = GetMapLaneByIdImproved(-999);
+  if (another_lane_ptr_maybe == std::nullopt){
+    // nullcheck , maybe early exit 
+  }
+//
+  
   // DONE Checking, do rest of the work
 }
 // What is not covered
--- clang-tools-extra/clang-tidy/misc/XcheckCompoundMatchCheck.cpp
+++ clang-tools-extra/clang-tidy/misc/XcheckCompoundMatchCheck.cpp
@@ -36,12 +36,19 @@ XcheckCompoundMatchCheck::XcheckCompoundMatchCheck(StringRef Name,ClangTidyConte
 void XcheckCompoundMatchCheck::registerMatchers(MatchFinder *Finder) {
   //TODO use pass in for function name
   for(const auto& TargetFunctionName : TargetFunctionNames){
+    // match: auto results = TargetFunctionName()
+    // match: auto results_maybe = TargetFunctionNameReturnsOptional()
+
+    // Match the invoke
     auto invoke_expr =  callExpr(hasDescendant(declRefExpr(to(functionDecl(hasName(TargetFunctionName)))))).bind("invoke_expr");
+    // back track to get entire function body    
     auto ancestor_func = functionDecl().bind("function_body");
+    // match the auto result = function part
+    // so that we can get the name "result" for further analysis
     auto decl_invoke    = declStmt(hasDescendant(varDecl().bind("var_decl")),
                                    hasDescendant(invoke_expr),
                                    hasAncestor(ancestor_func));
-  
+    
     Finder->addMatcher(decl_invoke, this);
   }
 }
@@ -52,13 +59,18 @@ void XcheckCompoundMatchCheck::check(const MatchFinder::MatchResult &Result) {
   const auto *var_decl       = Result.Nodes.getNodeAs<VarDecl>("var_decl");  
   const auto *function_body  = Result.Nodes.getNodeAs<FunctionDecl>("function_body");
 
+  // report if we found a function invoke from .tidy_config
   llvm::StringRef var_decl_name = var_decl->getName();
   diag(var_decl->getBeginLoc(), "matched compound stmt, var name [%0]")
     << var_decl_name;
 
+  // Raw AST Traversal to 
   // look for if statement that containts the var_decl_name
   const auto* Body = dyn_cast<CompoundStmt>(function_body->getBody());
   auto  has_direct_usage_of_var = declRefExpr(to(varDecl(hasName(var_decl_name))));
+  auto  used_nullopt            = declRefExpr(to(varDecl(hasName("nullopt"))));
+
+  // if we do not found if (!result)  if (result==std::nullopt)
   auto  negate_op = unaryOperator(hasOperatorName("!"));
   int matched_if = 0;
   for(const auto* S : Body->body()) {  
@@ -82,11 +94,18 @@ void XcheckCompoundMatchCheck::check(const MatchFinder::MatchResult &Result) {
        //     << matches_p2.size() ; 
        // }
        matched_if += matches_p2.size() ;
-              
+
+       const auto matches_p3 = match(ifStmt(hasDescendant(has_direct_usage_of_var),
+                                            hasDescendant(used_nullopt),                                            
+                                            hasDescendant(cxxOperatorCallExpr())),*if_stmt,*Result.Context);      
+       
+
+       matched_if += matches_p3.size() ;
+       
     }
   }  
   if( matched_if == 0 ){
-    diag(var_decl->getBeginLoc(), " Missing null check for %0 ")
+    diag(var_decl->getBeginLoc(), " Missing null check for ==> %0 ")
       << var_decl_name;
   }
 
